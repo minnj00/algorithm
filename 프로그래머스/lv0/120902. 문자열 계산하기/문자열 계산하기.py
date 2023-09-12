30893cd960d84c777c6bd0472afda0b19d4187cb
Minju Kang <140361656+minnj00@users.noreply.github.com>
2023-09-12 19:55:40 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,12 +1,15 @@
 def solution(my_string):
-    numcal = my_string.split()
-    result = int(numcal[0])
-    for i in range(1,len(numcal)-1, 2):
-        print(numcal[i])
-        if numcal[i] == '+':
-            result += int(numcal[i+1])
-        else:
-            result -= int(numcal[i+1])
+    # numcal = my_string.split()
+    # result = int(numcal[0])
+    # for i in range(1,len(numcal)-1, 2):
+    #     print(numcal[i])
+    #     if numcal[i] == '+':
+    #         result += int(numcal[i+1])
+    #     else:
+    #         result -= int(numcal[i+1])
+    # return result
+    
+    nums = map(int, my_string.replace(' - ', ' + -').split(' + '))     
+    result = sum(nums)
     return result
-            
         
