57d2f712cd7dc1df3537a93a43302fbb67ce8844
Minju Kang <140361656+minnj00@users.noreply.github.com>
2023-09-13 21:22:34 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,3 +1,4 @@
 def solution(my_string, is_prefix):
-#is_prefix의 길이에 만큼 my_string에서 슬라이싱 했을 때 둘이 같다면 1
+# is_prefix의 길이에 만큼 my_string에서 슬라이싱 했을 때 둘이 같다면 1
+# 스트링 슬라이싱 가능
     return 1 if my_string[:len(is_prefix)]==is_prefix else 0
