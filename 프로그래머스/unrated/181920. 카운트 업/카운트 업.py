58709ee2c02b16bedbe00957b18fbcd05d312954
Minju Kang <140361656+minnj00@users.noreply.github.com>
2023-09-13 21:15:15 +0900

    [unrated] Title: 카운트 업, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.py
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.py
@@ -1,6 +1,8 @@
 def solution(start_num, end_num):
-    list = []
-    for i in range(end_num-start_num+1):
-        list.append(start_num)
-        start_num += 1
-    return list
+    # list = []
+    # for i in range(end_num-start_num+1):
+    #     list.append(start_num)
+    #     start_num += 1
+    # return list
+    
+    return list(range(start_num, end_num+1))
